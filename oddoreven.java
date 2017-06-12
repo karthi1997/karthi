c215e8147e59a532a14220ee8ab7944e66c12c3f
karthi1997 <karthiselvam.777@gmail.com>
2017-06-12 15:32:32 +1100

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class oddoreven
+{
+public static void main(String[] arg)
+{
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+if(n%2==0)
+  System.out.println("even");
+  else
+   System.out.println("odd");
+   }
+   }
